dc5d1ad5ab0187092ac7d27ff2857eb56e39e746
tyno43 <skriulle5@gmail.com>
2018-10-29 11:08:52 +0900

    update: motor protocol test

--- SerialConnectionTest/Assets/Test/Editor/MotorHandlerTest.cs
+++ SerialConnectionTest/Assets/Test/Editor/MotorHandlerTest.cs
@@ -5,56 +5,97 @@ using System.Collections;
 
 public class NewTestScript
 {
-    private MotorHandler motorHandler;
-    // 8bitデータで駆動するので、動作させるモータは4つ
-    const int MOTOR_NUM = 4;
+    static MotorHandler motorHandler;
 
     [OneTimeSetUp]
-    public void OneTimeSetUp()
+    public static void OneTimeSetUp()
     {
-        motorHandler = new MotorHandler(MOTOR_NUM);
+        motorHandler = new MotorHandler(4);
     }
 
     [SetUp]
-    public void SetUp()
+    public static void SetUp()
     {
         motorHandler.Reset();
     }
 
-    [Test]
-    public void SetUpTest ()
+    [Description("指定したモーターの駆動強度を得る")]
+    public class ActivatePartTest
     {
-        Assert.AreEqual(MOTOR_NUM, motorHandler.motors.Length);
+        public void OneTimeSetUp_()
+        {
+            OneTimeSetUp();
+        }
+
+        [SetUp]
+        public void SetUp_()
+        {
+            SetUp();
+        }
+
+        [Test, Description("駆動を変更していない箇所は停止している")]
+        public void IsStopTest()
+        {
+            Assert.AreEqual(
+                Power.Stop,
+                motorHandler.PartPower(Side.Left, BodyPart.Chest)
+            );
+            Assert.AreEqual(
+                Power.Stop,
+                motorHandler.PartPower(Side.Right, BodyPart.Shoulder)
+            );
+        }
+
+        [Test, Description("最後に指定された強度で駆動する")]
+        public void StrongthTest()
+        {
+            motorHandler.Activate(Side.Right, BodyPart.Shoulder, Power.Strong);
+            Assert.AreEqual(Power.Strong, motorHandler.PartPower(Side.Right, BodyPart.Shoulder));
+
+            motorHandler.Activate(Side.Left, BodyPart.Chest, Power.Weak);
+            Assert.AreEqual(Power.Weak, motorHandler.PartPower(Side.Left, BodyPart.Chest));
+
+            motorHandler.Activate(Side.Right, BodyPart.Shoulder, Power.Weak);
+            Assert.AreEqual(Power.Weak, motorHandler.PartPower(Side.Right, BodyPart.Shoulder));
+        }
     }
 
-    [Test]
-    public void ActivatePartTest ()
+    [Description("送信するByteデータをテストする")]
+    public class GetByteDataTest
     {
-        motorHandler.Activate(Side.Right, BodyPart.Shoulder, Power.Strong);
-        Assert.AreEqual(Power.Strong, motorHandler.PartPower(Side.Right, BodyPart.Shoulder));
-        Assert.AreEqual(Power.Stop, motorHandler.PartPower(Side.Left, BodyPart.Chest));
-        motorHandler.Activate(Side.Left, BodyPart.Chest, Power.Weak);
-        Assert.AreEqual(Power.Weak, motorHandler.PartPower(Side.Left, BodyPart.Chest));
-    }
+        [OneTimeSetUp]
+        public void OneTimeSetUp_()
+        {
+            OneTimeSetUp();
+        }
 
-    [Test]
-    public void ActivaPartValueTest()
-    {
-        // 位置と強さを指定してできる値の正当性を検証
-        // [右肩、右胴、左肩、左胴]の順に2bitずつ割り当てている
-        // 2bitのうち左のほうが強、右が弱
-        Assert.AreEqual(System.Convert.ToByte("00000000", 2), motorHandler.GetValue());
+        [SetUp]
+        public void SetUp_()
+        {
+            SetUp();
+        }
 
-        motorHandler.Activate(Side.Right, BodyPart.Shoulder, Power.Strong);
-        Assert.AreEqual(System.Convert.ToByte("10000000", 2), motorHandler.GetValue());
+        [Test, Description("停止->00、弱->01、強->10")]
+        public void ActivaPartValueTest()
+        {
+            Assert.AreEqual(0, motorHandler.GetValue());
+            motorHandler.Activate(Side.Left, BodyPart.Chest, Power.Weak);
+            Assert.AreEqual(1, motorHandler.GetValue());
+            motorHandler.Activate(Side.Left, BodyPart.Chest, Power.Strong);
+            Assert.AreEqual(2, motorHandler.GetValue());
+        }
 
-        motorHandler.Activate(Side.Right, BodyPart.Shoulder, Power.Weak);
-        Assert.AreEqual(System.Convert.ToByte("01000000", 2), motorHandler.GetValue());
+        [Test, Description("部位ごとの駆動強度を変更すると、組み合わせで表示される")]
+        public void ActivePartsValueTest()
+        {
+            motorHandler.Activate(Side.Right, BodyPart.Shoulder, Power.Strong);
+            Assert.AreEqual(System.Convert.ToByte("10000000", 2), motorHandler.GetValue());
 
-        motorHandler.Activate(Side.Left, BodyPart.Chest, Power.Strong);
-        motorHandler.Activate(Side.Left, BodyPart.Shoulder, Power.Weak);
-        motorHandler.Activate(Side.Right, BodyPart.Chest, Power.Strong);
-        motorHandler.Activate(Side.Right, BodyPart.Shoulder, Power.Stop);
-        Assert.AreEqual(System.Convert.ToByte("00100110", 2), motorHandler.GetValue());
+            motorHandler.Activate(Side.Left, BodyPart.Chest, Power.Strong);
+            motorHandler.Activate(Side.Left, BodyPart.Shoulder, Power.Weak);
+            motorHandler.Activate(Side.Right, BodyPart.Chest, Power.Strong);
+            motorHandler.Activate(Side.Right, BodyPart.Shoulder, Power.Stop);
+            Assert.AreEqual(System.Convert.ToByte("00100110", 2), motorHandler.GetValue());
+        }
     }
 }
