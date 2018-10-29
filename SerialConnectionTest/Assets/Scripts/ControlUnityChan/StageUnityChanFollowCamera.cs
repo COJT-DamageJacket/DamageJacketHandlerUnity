a1321272b78f3969dbad0a7bba043bd0a488ac99
tyno43 <skriulle5@gmail.com>
2018-10-29 22:08:51 +0900

    add: proto of first game

---
+++ SerialConnectionTest/Assets/Scripts/ControlUnityChan/StageUnityChanFollowCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class StageUnityChanFollowCamera : MonoBehaviour {
+
+    const float RADIUS = 17.0f;
+
+    private Transform trans;
+    [SerializeField] private StageCircleRun stageCircleRun;
+    [SerializeField] private ElevateStage elevateStage;
+
+	// Use this for initialization
+	void Start () {
+        trans = transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float theta = stageCircleRun.theta;
+        float r = theta / 180 * Mathf.PI;
+
+        trans.rotation = Quaternion.AngleAxis((float)(-90 + theta), new Vector3(0, 1, 0));
+        trans.position = new Vector3(Mathf.Cos(r) * RADIUS, elevateStage.height+4, -Mathf.Sin(r) * RADIUS);
+    }
+}
---
+++ SerialConnectionTest/Assets/Scripts/GameObject/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    const float TIMELIMIT = 1.2f;
+    const float VELOCITY = 10;
+
+    private Timer timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = new Timer();
+        timer.expire += () =>{ Destroy(gameObject); };
+	}
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer.UpdateTime(Time.deltaTime);
+    }
+
+    public void SetVelocity(float theta)
+    {
+        timer.Start(TIMELIMIT);
+
+        float r = theta / 180 * Mathf.PI;
+        GetComponent<Rigidbody>().velocity = new Vector3(-Mathf.Cos(r), 0, Mathf.Sin(r)) * VELOCITY;
+    }
+}
